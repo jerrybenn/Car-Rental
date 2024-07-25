67d217c445944f3b27961a8cb9bb042e5b2655e4
Jerry Benn <jerrydarobenn@gmail.com>
2024-07-25 17:27:29 -0400

    added base navbar

--- car-rental/app/components/Navbar.tsx
+++ car-rental/app/components/Navbar.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import './Navbar.css'; // Importing the CSS file
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="navbar">
+      <div className="logo">
+        <Link href="/">Heritage Car Rentals</Link>
+      </div>
+      <div className="navLinks">
+        <Link href="/about">About Us</Link>
+        <Link href="/services">Services</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
