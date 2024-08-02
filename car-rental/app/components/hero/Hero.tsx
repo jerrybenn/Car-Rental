01a63aaf35657301b89daf99f880df92ace63b70
Jerry Benn <jerrydarobenn@gmail.com>
2024-08-02 11:43:15 -0400

    hero text

--- car-rental/app/components/hero/Hero.tsx
+++ car-rental/app/components/hero/Hero.tsx
@@ -5,7 +5,18 @@ import './hero.css'; // Importing the CSS file
 const Hero = () => {
   return (
     <main className="hero-container">
-        <div className="hero-text">tex</div>
+        <div className='hero-fonts'>
+            <div className="hero-text">
+                <h1>Premium</h1>
+                <h1>Car Rental</h1>
+                <h1>in New York</h1>
+            </div>
+            <div className='hero-subtext'>
+                <p> Don't deny yourself the pleasure of</p>
+                <p>driving the best premium cars from</p>
+                <p>around the world here and now</p>
+            </div>
+        </div>
         <div className="hero-image">imageree</div>
     </main>
   );
--- car-rental/app/page.tsx
+++ car-rental/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import Hero from "./components/hero/hero";
+import Hero from "./components/hero/Hero";
+
 
 
 export default function Home() {
